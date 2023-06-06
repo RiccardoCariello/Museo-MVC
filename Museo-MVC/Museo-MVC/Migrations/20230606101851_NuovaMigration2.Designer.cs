656c174c58f2dc4ee43dec8e98407628183a93d8
MillionDollarBob <riccardo.c1995@outlook.com>
2023-06-06 12:19:40 +0200

    autentication

--- Museo-MVC/Museo-MVC/Migrations/20230606101851_NuovaMigration2.Designer.cs
+++ Museo-MVC/Museo-MVC/Migrations/20230606101851_NuovaMigration2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Museo_MVC.DataBase;
 
@@ -11,9 +12,11 @@ using Museo_MVC.DataBase;
 namespace Museo_MVC.Migrations
 {
     [DbContext(typeof(MuseoContext))]
-    partial class MuseoContextModelSnapshot : ModelSnapshot
+    [Migration("20230606101851_NuovaMigration2")]
+    partial class NuovaMigration2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
