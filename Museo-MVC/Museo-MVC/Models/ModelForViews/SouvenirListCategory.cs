7c8838acd468fa28a936e0c504af2d227bd64282
UMBY2407 <umby2407@gmail.com>
2023-06-06 11:16:22 +0200

    Added Souvenir, Category e ModelForViews Models

---
+++ Museo-MVC/Museo-MVC/Models/ModelForViews/SouvenirListCategory.cs
@@ -0,0 +1,8 @@
+﻿namespace Museo_MVC.Models.ModelForViews
+{
+	public class SouvenirListCategory
+	{
+		public Souvenir Souvenirs { get; set; }
+		public List<Category>? Categories { get; set; }
+	}
+}
