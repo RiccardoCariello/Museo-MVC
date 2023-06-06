5f505754baa6d36ff0141e417a2fe5b5c0cbde5e
MillionDollarBob <riccardo.c1995@outlook.com>
2023-06-06 10:58:02 +0200

    Context Creato

---
+++ Museo-MVC/Museo-MVC/DataBase/MuseoContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Museo_MVC.Models;
+
+
+namespace Museo_MVC.DataBase
+{
+    public class MuseoContext : DbContext
+    {
+
+
+        public DbSet<Souvenir> Souvenir { get ; set };
+
+        public DbSet<Category> Category { get; set; }
+
+
+
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=EFMuseo;Integrated Security=True");
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=EFTecBlog;" + "Integrated Security=True;TrustServerCertificate=True");
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
