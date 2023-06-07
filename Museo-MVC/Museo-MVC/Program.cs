b9b06e8aca74d7e557bfb14a57e1e9f41a62fa44
MillionDollarBob <riccardo.c1995@outlook.com>
2023-06-07 17:42:00 +0200

    niente di che

--- Museo-MVC/Museo-MVC/Program.cs
+++ Museo-MVC/Museo-MVC/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Museo_MVC.DataBase;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("MuseoContextConnection") ?? throw new InvalidOperationException("Connection string 'MuseoContextConnection' not found.");
+//var connectionString = builder.Configuration.GetConnectionString("MuseoContextConnection") ?? throw new InvalidOperationException("Connection string 'MuseoContextConnection' not found.");
 
 
 
