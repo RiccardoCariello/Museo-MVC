98833179b5717b4b1c5f9e77a7dcbb85d1290a06
MillionDollarBob <riccardo.c1995@outlook.com>
2023-06-08 11:55:45 +0200

    grandi bestemmie

--- Museo-MVC/Museo-MVC/Migrations/20230608094557_NewTablesAcquistis5TheRevenge.Designer.cs
+++ Museo-MVC/Museo-MVC/Migrations/20230608094557_NewTablesAcquistis5TheRevenge.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Museo_MVC.DataBase;
 
@@ -11,9 +12,11 @@ using Museo_MVC.DataBase;
 namespace Museo_MVC.Migrations
 {
     [DbContext(typeof(MuseoContext))]
-    partial class MuseoContextModelSnapshot : ModelSnapshot
+    [Migration("20230608094557_NewTablesAcquistis5TheRevenge")]
+    partial class NewTablesAcquistis5TheRevenge
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
