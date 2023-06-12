7413c7a7e0c5473347663316e8f0741baf83bb16
MillionDollarBob <riccardo.c1995@outlook.com>
2023-06-12 09:20:31 +0200

    creata classe Ordini

---
+++ Museo-MVC/Museo-MVC/Models/Ordini.cs
@@ -0,0 +1,14 @@
+﻿namespace Museo_MVC.Models
+{
+    public class Ordini
+    {
+        public int Id {get; set;}
+
+        public string Name { get; set;}
+
+        public string surname { get; set;}
+
+
+
+    }
+}
